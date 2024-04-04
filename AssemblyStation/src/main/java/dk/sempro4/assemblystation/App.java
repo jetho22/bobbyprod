2c710a69c4de02677793334226eb8c0ae6356144
Jeppe <jetho22@student.sdu.dk>
2024-04-04 11:59:16 +0200

    first commit

---
+++ AssemblyStation/src/main/java/dk/sempro4/assemblystation/App.java
@@ -0,0 +1,13 @@
+package dk.sempro4.assemblystation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Warehouse/src/main/java/dk/sempro4/warehouse/App.java
@@ -0,0 +1,13 @@
+package dk.sempro4.warehouse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
