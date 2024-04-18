d83aa3a990ca2524975db269f94c7bee2cbdcf06
SjuChefen <madsbraae94@gmail.com>
2024-04-18 11:17:47 +0200

    Added branches and class to assembly

---
+++ AssemblyStation/src/main/java/dk/sempro4/assemblystation/ASClient.java
@@ -0,0 +1,6 @@
+package dk.sempro4.assemblystation;
+
+public class ASClient
+{
+
+}
--- AssemblyStation/src/test/java/dk/sempro4/assemblystation/ASClientTest.java
+++ AssemblyStation/src/test/java/dk/sempro4/assemblystation/ASClientTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class ASClientTest
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public ASClientTest(String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( ASClientTest.class );
     }
 
     /**
